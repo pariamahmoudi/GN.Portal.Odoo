6937ff37cbeb084aabe680a9d6225cc2c11fb829
pariamahmoudi <paria.mahmoudi@gmail.com>
2020-10-26 09:55:51 +0330

    last

--- Addons/gn_portal/models/SaleOrderExtensions.py
+++ Addons/gn_portal/models/SaleOrderExtensions.py
@@ -4,7 +4,6 @@ from ..imports import fields,models,SaleOrder,SaleOrderLine,api , GregorianToJal
 
 class SaleOrderExtensions(SaleOrder):
     _inherit = 'sale.order'
-    gn_additional_note = fields.Text(default="اعتبار این پیش‌فاکتور به مدت یک هفته می‌باشد\n در صورت حضور کارشناس در محل مشتری هزینه ایاب و ذهاب به عهده مشتری است \n در صورت ارائه خدمات به میزان کمتر از دو ساعت صورت حساب با حداقل دو ساعت صادر می‌گردد\n برای خریدهای کمتر از ده میلیون ریال هزینه ارسال به عهده مشتری است")
     gn_reciever = fields.Many2one('res.partner')
     gn_calculate_date = fields.Char(compute="calculate_date")
      
