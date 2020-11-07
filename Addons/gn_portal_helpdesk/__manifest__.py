430e5a630e2ebe7c33293784525fa7b4b837218a
pariamahmoudi <paria.mahmoudi@gmail.com>
2020-11-07 14:10:05 +0330

    helpdesk created

---
+++ Addons/gn_portal_helpdesk/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "gn.portal.helpdesk",
+
+    'summary': """
+        Gostareh Negar helpdesk Extensions
+    """,
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base',],
+
+    # always loaded
+    'data': [
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+
+    ],
+    'application': True,
+    'installable': True,
+}
